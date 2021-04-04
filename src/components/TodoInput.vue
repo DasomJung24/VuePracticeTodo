6c860ac2596bda4e1663a2ed60d964e08a4992ef
DasomJung24 <jeongdasom6@gmail.com>
2021-04-04 18:02:33 +0900

    props emit 구현

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -19,10 +19,9 @@ export default {
         addTodo() {
             if (this.newTodoItem !== "") {
                 var value = this.newTodoItem && this.newTodoItem.trim();
-                localStorage.setItem(value, value);
+                this.$emit('addTodo', value);
                 this.clearInput();
             }
-            
         },
         clearInput() {
             this.newTodoItem = '';
